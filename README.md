ec708e0552e14898202b36f9784c67a95310586f
Scorpion-rep <86839837+Scorpion-rep@users.noreply.github.com>
2021-09-25 12:09:07 +0800

    css and js completed

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Daily-Schedule
+
+# HTML Modification
+    1. Created 8 div for timeblocks of the work day schedule.
+    2. Each div has 'id' and 'class' and contains the children:
+        + A div to display the timeblock for standard business hours.
+        + A textarea for text input, sized with 9 column, with class is added corresponded with the selector '.description' in css.
+        + A button contained <i> tag with an icon fas fa-save to indicate it's a save button.
+    3.All children div are wrapped in the div .class container.    
+# CSS Modification
+    1. Added font-display to body
+    2. Added vertical align to textarea
+    3. Added display flex to selector time-block
+    4. Added values to selector hour (details in css)
+    5. Added hover method in each timeblock 'past, present and future'.
+# JavaScript
+    Descriptions are in JS file.
+
+# User experience
+    1. When starting the page, the planner displayed with current day and time at the top of the calendar.
+    2. When scrolling down, user is presented with timeblocks with standard business working hours.
+    3. When user views the timeblocks, the past hours are coded grey, current timeblock is red and the blocks for future time indicated with green color.
+    4. User can enter the event in each block and save by blue button.
+    5. The event is saved in local storage to recall for next time when calendar is used (events persist when refresh the page)
+              
--- script.js
+++ script.js
@@ -1,21 +1,68 @@
-const timeDisplayEl = $('#currentDay');
+let timeDisplayEl = $('#currentDay');
 
-
-
-
-//Open the planner => displaying current time /date
+// get current time and date
 function displayTime() {
-  var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss');
-  timeDisplayEl.text(rightNow);
+  let currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss');
+  timeDisplayEl.text(currentTime);
 }
 setInterval(displayTime, 1000);
 
-// Each timeblock is color coded for past, present or future
+// set function to grab the event and store the document in local storage
+$(document).ready(function() {
 
-// Click on timeblock => Enter event
-// Click on save btn 
+  // save btn event listen to store events in local storage
+  $('.saveBtn').on('click', function (event) {
+    event.preventDefault();
+    //get values 
+    let text = $(this).siblings('.description').val();
+    let time = $(this).parent().attr('id');    
+    
+    //data is saved in local storage
+    localStorage.setItem(time, text);
+  })
 
-//=> Event is saved in local Storage
+  function trackingTime() {
+ 
+    // get current time/hour
+    let timeNow = moment().hour();
 
-//When refresh the page 
-//=> saved event persist
+    //loop over time blocks
+    $('.time-block').each(function () {
+      let blockTime = parseInt($(this).attr('id').split('-')[1]);
+
+      // Conditions to generate the background color code      
+      if (blockTime < timeNow) {
+        $(this).removeClass('future');
+        $(this).removeClass('present');
+        $(this).addClass('past');
+
+      }
+      else if (blockTime === timeNow) {
+        $(this).removeClass('past');
+        $(this).removeClass('future');
+        $(this).addClass('present');
+      }
+      else {
+        $(this).removeClass('present');
+        $(this).removeClass('past');
+        $(this).addClass('future');
+      }
+
+    })
+
+  }
+
+  // Getting items from local storage
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+  trackingTime();
+})
+
